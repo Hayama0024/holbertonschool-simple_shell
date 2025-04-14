d48de55fd924096642f3724c6952f07c70a632d4
Hayama0024 <hayamareizo11@gmail.com>
2025-04-14 14:04:23 +0000

    add display_prompt function

---
+++ utils.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+/**
+ * display_prompt - Prints the shell prompt to the standard output
+ * This function displays a simple "$ " prompt on the terminal,
+ * inviting the user to type a command.
+ */
+void display_prompt(void)
+{
+	write(STDOUT_FILENO, "$ ", 2);
+}
