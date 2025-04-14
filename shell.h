2ba4dad179bdb6de39f54cc706bd64987dcf0768
Hayama0024 <hayamareizo11@gmail.com>
2025-04-14 12:59:45 +0000

    creat .h files with needed prototypes for the entire project

---
+++ shell.h
@@ -0,0 +1,30 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+/* ライブラリ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+/* 環境変数 */
+extern char **environ;
+
+/* プロンプトと入力 */
+void display_prompt(void);
+char *read_input(void);
+
+/* コマンドの分割と実行 */
+char **split_line(char *line);
+int execute_command(char **args);
+
+/* メモリ解放やユーティリティ */
+void free_args(char **args);
+void print_error(char *prog_name, char *cmd);
+
+#endif /* SHELL_H */
+
