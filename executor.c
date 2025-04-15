272c65169b5c8701d37de080f78145f6279db7a9
Emi-H106 <emi.hatano@yahoo.com>
2025-04-15 10:58:44 +0200

    fix initialization of empty_args

--- executor.c
+++ executor.c
@@ -31,7 +31,9 @@ int execute_command(char **args)
 	if (pid == 0) /*if the current prosess is a child process*/
 	{
 		/* Pass only the command without additional arguments */
-		char *empty_args[] = {args[0], NULL};
+		char *empty_args[2];
+		empty_args[0] = args[0];
+		empty_args[1] = NULL;	
 
 		/*run without additional arguments*/
 		if (execve(args[0], empty_args, environ) == -1)
@@ -42,7 +44,7 @@ int execute_command(char **args)
 	}
 	else /*for the parent process*/
 	{
-		waitpid(pid, &status, 0);/*wait for child process to finish*/
+		waitpid(pid, &status, 0); /*wait for child process to finish*/
 	}
 
 	return (1);/*continuing the shell*/
