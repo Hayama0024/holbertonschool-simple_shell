1af6d501255994bc501ac5e4cb963cd6d69df910
Hayama0024 <hayamareizo11@gmail.com>
2025-04-16 17:45:25 +0000

    fix error code 127

--- executor.c
+++ executor.c
@@ -12,50 +12,53 @@
 
 int execute_command(char **args)
 {
-	pid_t pid;
-	int status;
-	char *cmd_path = NULL;
-	struct stat st;
+pid_t pid;
+int status;
+char *cmd_path = NULL;
+
+	if (args[0] == NULL)
+		return (1);
 
 	if (args[0][0] == '/' || args[0][0] == '.')
 	{
-		if (stat(args[0], &st) != 0)
-		{
-			fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
-			return (1);
-		}
-		cmd_path = strdup(args[0]);
+		if (access(args[0], X_OK) == 0)
+			cmd_path = strdup(args[0]);
 	}
 	else
 	{
-		cmd_path = which_path(args[0]);
-		if (!cmd_path)
-		{
-			fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
-			return (1);
-		}
+		cmd_path = which_path(args[0]);  /* custom implementation */
+	}
+
+	if (cmd_path == NULL)
+	{
+		dprintf(STDERR_FILENO, "./hsh: 1: %s: not found\n", args[0]);
+		return (1);  /* no fork*/
 	}
 
 	pid = fork();
 	if (pid == -1)
 	{
 		perror("fork");
-		free(cmd_path);
-		return (-1);
+	free(cmd_path);
+	return (-1);
 	}
 
 	if (pid == 0)
 	{
-		execve(cmd_path, args, environ);
-		perror("execve");
-		exit(EXIT_FAILURE);
+		/* Child process */
+		if (execve(cmd_path, args, environ) == -1)
+		{
+			perror("execve");
+			free(cmd_path);
+			exit(127);
+		}
 	}
 	else
 	{
+		/* Parent process */
 		waitpid(pid, &status, 0);
 	}
 
 	free(cmd_path);
 	return (1);
 }
-
