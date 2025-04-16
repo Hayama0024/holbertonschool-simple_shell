28fb4083a318bab33800af11928f54bf722774c1
Hayama0024 <hayamareizo11@gmail.com>
2025-04-16 19:30:50 +0000

    reset previous commits, working in main branch without notice

--- executor.c
+++ executor.c
@@ -1,58 +1,57 @@
 #include "shell.h"
-
-
-/**
-* execute_command - Executes a command using fork and execve
-* @args: an array of strings containing
-*       the command (args[0]) to execute
-* Return: 1 always
-*        (indicates the shell should continue running)
-*/
-
-
-int execute_command(char **args)
-{
-pid_t pid;
-int status;
-char *cmd_path = NULL;
-
-	if (access(args[0], X_OK) == 0)
-	{
-			cmd_path = strdup(args[0]);
-	}
-	else
-	{
-		cmd_path = which_path(args[0]);  /* custom implementation */
-	}
-
-	if (!cmd_path)
-	{
-		write(STDERR_FILENO, "./hsh: 1: ", 10);
-		write(STDERR_FILENO, args[0], strlen(args[0]));
-		write(STDERR_FILENO, ": not found\n", 12);
-		_exit(127);
-	}
-
-	pid = fork();
-	if (pid == -1)
-	{
-		perror("fork");
-		free(cmd_path != args[0] ? cmd_path : NULL);
-		return (-1);
-	}
-
-	if (pid == 0)/*child process*/
-	{
-		execve(cmd_path, args, environ);
-		perror("execve");
-		exit(1);
-	}
-	else/*parent process*/
-	{
-		waitpid(pid, &status, 0);
-	}
-	if (cmd_path != args[0])
-		free(cmd_path);
-
-	return (1);
-}
+ 
+ 
+ /**
+  * execute_command - Executes a command using fork and execve
+  * @args: an array of strings containing
+  *       the command (args[0]) to execute
+  * Return: 1 always
+  *        (indicates the shell should continue running)
+  */
+ 
+ 
+ int execute_command(char **args)
+ {
+ 	pid_t pid;
+ 	int status;
+ 	char *cmd_path;
+ 
+ 	if (args[0] == NULL) /*in case of an empty command*/
+ 	{
+ 		return (1);
+ 	}
+ 
+ 	cmd_path = which_path(args[0]);
+ 	if (cmd_path == NULL)
+ 	{
+ 		perror("Command not found");
+ 		return (1);
+ 	}
+ 
+ 	pid = fork();
+ 
+ 	if (pid == -1)
+ 	{
+ 		perror("fork");
+ 		return (-1);
+ 	}
+ 
+ 	if (pid == 0) /*if the current prosess is a child process*/
+ 	{
+ 		if (execve(args[0], args, environ) == -1)
+ 		if (execve(cmd_path, args, environ) == -1)
+ 		{
+ 			perror("Error");
+ 			perror("execve");
+ 			free(cmd_path);
+ 			exit(EXIT_FAILURE);
+ 		}
+ 	}
+ 	else /*for the parent process*/
+ 	{
+ 		waitpid(pid, &status, 0); /*wait for child process to finish*/
+ 	}
+ 
+ 	free(cmd_path);
+ 	return (1);/*continue the shell*/
+ }
--- utils.c
+++ utils.c
@@ -1,65 +1,71 @@
 #include "shell.h"
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-/**
-* display_prompt - Prints the shell prompt to the standard output
-* This function displays a simple "$ " prompt on the terminal,
-* inviting the user to type a command.
-*/
-void display_prompt(void)
-{
-	write(STDOUT_FILENO, "$ ", 2);
-}
-/**
-* free_args - frees args
-* @args:array of characters
-*/
-void free_args(char **args)
-{
-	if (args)
-		free(args);
-}
-/**
-* which_path - Searches for a command in
-* the directories listed in PATH
-* @command: The command name (e.g., "ls") to search for
-* Return: A pointer to the full path string if found (must be freed),
-*         or NULL if the command is not found.
-*/
-
-char *which_path(char *command)
-{
-	char *path_env, *path_copy, *dir, *full_path;
-	struct stat st;
-
-path_env = getenv("PATH");
-    if (!path_env)
-        return (NULL);
-
-    path_copy = strdup(path_env);
-    if (!path_copy)
-        return (NULL);
-
-    dir = strtok(path_copy, ":");
-    while (dir)
-    {
-        full_path = malloc(strlen(dir) + strlen(command) + 2);
-        if (!full_path)
-        {
-            free(path_copy);
-            return (NULL);
-        }
-        sprintf(full_path, "%s/%s", dir, command);
-        if (stat(full_path, &st) == 0)
-        {
-            free(path_copy);
-            return (full_path);
-        }
-        free(full_path);
-        dir = strtok(NULL, ":");
-    }
-
-    free(path_copy);
-    return (NULL);
-}
+ #include <stdio.h>
+ #include <unistd.h>
+ #include <stdlib.h>
+ /**
+  * display_prompt - Prints the shell prompt to the standard output
+  * This function displays a simple "$ " prompt on the terminal,
+  * inviting the user to type a command.
+  */
+ void display_prompt(void)
+ {
+ 		write(STDOUT_FILENO, "$ ", 2);
+ }
+ /**
+  * free_args - frees args
+  * @args:array of characters
+  */
+ void free_args(char **args)
+ {
+ 	if (args != NULL)
+ 		free(args);
+ }
+ /**
+  * which_path - Searches for a command in
+  * the directories listed in PATH
+  * @command: The command name (e.g., "ls") to search for
+  * Return: A pointer to the full path string if found (must be freed),
+  *         or NULL if the command is not found.
+  */
+ 
+ char *which_path(char *command)
+ {
+ 	char *path_env = getenv("PATH");
+ 	char *path_copy, *dir, *full_path;
+ 	char *path_copy = NULL, *dir, *full_path;
+ 	struct stat st;
+ 
+ 	if (!path_env)
+ 		return (NULL);
+ 
+ 	if (command[0] == '/' || command[0] == '.')
+ 	{
+ 		if (stat(command, &st) == 0)
+ 			return (strdup(command));
+ 		return (NULL);
+ 	}
+ 
+ 	dir = strtok(path_copy, ":");
+ 	while (dir)
+ 	{
+ 		full_path = malloc(strlen(dir) + strlen(command) + 2);
+ 		if (!full_path)
+ 		{
+ 			free(path_copy);
+ 			return (NULL);
+ 		}
+ 
+ 		sprintf(full_path, "%s/%s", dir, command);
+ 
+ 		if (stat(full_path, &st) == 0)
+ 		{
+ 			free(path_copy);
+ 			return (full_path);
+ 		}
+ 
+ 		free(full_path);
+ 		dir = strtok(NULL, ":");
+ 	}
+ 	free(path_copy);
+ 	return (NULL);
+ }
