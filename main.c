44ddf2439252eed47551f1b0598936ef8d8f3557
Emi-H106 <emi.hatano@yahoo.com>
2025-04-18 14:51:29 +0200

    add code for exit

--- main.c
+++ main.c
@@ -22,13 +22,22 @@ int main(void)
 		{
 			if (isatty(STDIN_FILENO))
 				write(STDOUT_FILENO, "\n", 1);
-			
+
 			break;
 		}
 
 		args = split_line(line);
 		if (args[0] != NULL)
+		{
+			if (strcmp(args[0], "exit") == 0)
+			{
+				free(line);
+				free_args(args);
+				exit(0);
+			}
+
 			execute_command(args);
+		}
 
 		free(line);
 		free_args(args);
